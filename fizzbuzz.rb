76055965a181f152743af25302eb828b11f0c248
Christopher K Swofford <christopher.swofford@gmail.com>
2019-03-22 15:17:31 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,53 +1,10 @@
-
-
-require 'benchmark'
-require 'pry'
-
-class Fizzbuzz
-  def initialize(number)
-    @number = number
+numbers = 1..100
+numbers.each do |that_particular_number|
+  if that_particular_number % 3 == 0 && that_particular_number % 5 == 0
+    puts 'FIZZBUZZ'
+  elsif that_particular_number % 3 == 0
+    puts 'FIZZ'
+  elsif that_particular_number % 5 == 0
+    puts 'BUZZ'
   end
-
-  def self.count(number)
-    count = new(number)
-    count.output_data[count.selector]
-  end
-
-  def self.count_if(number)
-    count = new(number)
-    count.output_if
-  end
-
-  def output_if
-    result = 'Fizz' if @number % 3 == 0
-    result = 'Buzz' if @number % 5 == 0         
-    result = 'Fizzbuzz' if @number % 15 == 0
-    result 
-  end
-
-  def output_data
-    { "3" => "Fizz", 
-      "5" => "Buzz",
-      "15" => "Fizzbuzz" }
-  end
-
-  def selector
-    output_data.keys.map { |k| k.to_i }.select do |k| 
-      @number % k == 0 
-    end.last
-  end
-end
-
-Benchmark.bm(15) do |x|
-  x.report("if") { 
-    10000.times do 
-      Fizzbuzz.count_if(Random.rand(100))
-    end
-  }
-
-  x.report("hash_select") { 
-    10000.times do 
-      Fizzbuzz.count(Random.rand(100))
-    end
-  }
 end
