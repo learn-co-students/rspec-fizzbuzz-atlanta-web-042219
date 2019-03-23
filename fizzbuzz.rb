c7b01ac013c9d42f8193498b6fcafd7644d809b5
Christopher K Swofford <christopher.swofford@gmail.com>
2019-03-23 18:38:14 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,17 @@
-def fizz_buzz_1(max)
+def fizz_buzz_3(max)
   arr = []
   (1..max).each do |n|
-    if ((n % 3 == 0) && (n % 5 == 0))
-      arr << "FizzBuzz"
-    elsif (n % 3 == 0)
-      arr << "Fizz"
-    elsif (n % 5 == 0)
-      arr << "Buzz"
-    else
-      arr << n
+    text = ""
+    if (n % 3 == 0)
+      text << "Fizz"
     end
+    if (n % 5 == 0)
+      text << "Buzz"
+    end
+    if !((n % 3 == 0) || (n % 5 == 0))
+      text = n
+    end
+    arr << text
   end
   return arr
 end
