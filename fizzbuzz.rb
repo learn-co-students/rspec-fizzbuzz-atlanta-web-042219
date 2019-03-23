a10644839a957ba024d750c56bcb7bd34f390df1
Christopher K Swofford <christopher.swofford@gmail.com>
2019-03-23 18:47:33 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,7 @@
-p (1..100).map{|i|o="";o<<"Fizz" if i%3==0;o<<"Buzz" if i%5==0;o<<i.to_s if o=="";o}
+describe "fizzbuzz" do
+  it 'returns "Fizz" when the number is divisible by 3' do
+    fizz_3 = fizzbuzz(3)
+
+    expect(fizz_3).to eq("Fizz")
+  end
+end
