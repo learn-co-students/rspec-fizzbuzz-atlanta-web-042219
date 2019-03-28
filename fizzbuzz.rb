64f1ce1bd506ca10ad3297237570f72a0182e98d
han xu <hanxu27@gmail.com>
2019-03-28 21:39:05 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(value)
-  
+  if value % 15 == 0
+    puts ""
 end
