5a5b957b7c66b07974f2952570c3d37daa3c8111
han xu <hanxu27@gmail.com>
2019-03-28 21:45:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,5 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(value)
-  if value % 15 == 0
-    "FizzBuzz"
 
-  elsif value % 3 == 0
-    "Fizz"
-
-  elsif value % 5 == 0
-    "Buzz"
-
-  else
-    nil
 end
